a930b031f6a43e54021d5b7e733b76f52fee99f8
Va2 <vadim.brieven@gmail.com>
2019-04-16 15:21:25 +0200

    MENU: active link function

--- wp-content/themes/labs/includes/menu.php
+++ wp-content/themes/labs/includes/menu.php
@@ -12,3 +12,12 @@ class LabsMenu
     }
 }
 add_action('after_setup_theme', [LabsMenu::class, 'register_main_menu']);
+
+// ACTIVE MENU CLASS
+add_filter('nav_menu_css_class' , 'active_nav_class' , 10 , 2);
+function active_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
