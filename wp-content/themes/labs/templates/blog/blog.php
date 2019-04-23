5ca360502c29ac45eaf7abd72e04c647099d492b
Va2 <vadim.brieven@gmail.com>
2019-04-23 21:06:05 +0200

    Blog page (home.php): update to a dynamic page

--- wp-content/themes/labs/templates/blog/blog.php
+++ wp-content/themes/labs/templates/blog/blog.php
@@ -3,66 +3,40 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-sm-7 blog-posts">
-                <!-- Post item -->
-                <div class="post-item">
-                    <div class="post-thumbnail">
-                        <img src="<?= get_template_directory_uri() ?>/assets/img/blog/blog-2.jpg" alt="">
-                        <div class="post-date">
-                            <h2>03</h2>
-                            <h3>Nov 2017</h3>
+                <?php
+                // Nous allons faire en sorte d'aller chercher tout les articles pour les affichers sur la page
+                // Pour cela nous allons utiliser la class WP_Query 
+                // https://developer.wordpress.org/reference/classes/wp_query/
+                $args = [
+                    'post_type' => 'post',
+                    'posts_per_page' => 3,
+                ];
+                $query = new WP_Query($args);
+                while ($query->have_posts()) : $query->the_post();
+                ?>
+
+                    <!-- Post item -->
+                    <div class="post-item">
+                        <div class="post-thumbnail">
+                            <?php the_post_thumbnail(); ?>
+                            <div class="post-date">
+                                <h2><?= get_the_date('d'); ?></h2>
+                                <h3><?= get_the_date('M Y'); ?></h3>
+                            </div>
+                        </div>
+                        <div class="post-content">
+                            <h2 class="post-title"><?php the_title(); ?></h2>
+                            <div class="post-meta">
+                                <a href=""><?php the_author(); ?></a>
+                                <?php the_category(' '); //  CSS PROBLEM ON PAGE ?> 
+                                <a href=""><?php comments_number(); ?></a>
+                            </div>
+                            <p><?php the_excerpt(); ?></p>
+                            <a href="blog-post.html" class="read-more">Read More</a>
                         </div>
                     </div>
-                    <div class="post-content">
-                        <h2 class="post-title">Just a simple blog post</h2>
-                        <div class="post-meta">
-                            <a href="">Loredana Papp</a>
-                            <a href="">Design, Inspiration</a>
-                            <a href="">2 Comments</a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Phasellus vestibulum, quam tincidunt venenatis ultrices, est libero mattis ante, ac consectetur diam neque eget quam. Etiam feugiat augue et varius blandit. Praesent mattis, eros a sodales commodo.</p>
-                        <a href="blog-post.html" class="read-more">Read More</a>
-                    </div>
-                </div>
-                <!-- Post item -->
-                <div class="post-item">
-                    <div class="post-thumbnail">
-                        <img src="<?= get_template_directory_uri() ?>/assets/img/blog/blog-1.jpg" alt="">
-                        <div class="post-date">
-                            <h2>03</h2>
-                            <h3>Nov 2017</h3>
-                        </div>
-                    </div>
-                    <div class="post-content">
-                        <h2 class="post-title">Just a simple blog post</h2>
-                        <div class="post-meta">
-                            <a href="">Loredana Papp</a>
-                            <a href="">Design, Inspiration</a>
-                            <a href="">2 Comments</a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Phasellus vestibulum, quam tincidunt venenatis ultrices, est libero mattis ante, ac consectetur diam neque eget quam. Etiam feugiat augue et varius blandit. Praesent mattis, eros a sodales commodo.</p>
-                        <a href="blog-post.html" class="read-more">Read More</a>
-                    </div>
-                </div>
-                <!-- Post item -->
-                <div class="post-item">
-                    <div class="post-thumbnail">
-                        <img src="<?= get_template_directory_uri() ?>/assets/img/blog/blog-3.jpg" alt="">
-                        <div class="post-date">
-                            <h2>03</h2>
-                            <h3>Nov 2017</h3>
-                        </div>
-                    </div>
-                    <div class="post-content">
-                        <h2 class="post-title">Just a simple blog post</h2>
-                        <div class="post-meta">
-                            <a href="">Loredana Papp</a>
-                            <a href="">Design, Inspiration</a>
-                            <a href="">2 Comments</a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Phasellus vestibulum, quam tincidunt venenatis ultrices, est libero mattis ante, ac consectetur diam neque eget quam. Etiam feugiat augue et varius blandit. Praesent mattis, eros a sodales commodo.</p>
-                        <a href="blog-post.html" class="read-more">Read More</a>
-                    </div>
-                </div>
+                <?php endwhile; ?>
+
                 <!-- Pagination -->
                 <div class="page-pagination">
                     <a class="active" href="">01.</a>
@@ -70,6 +44,8 @@
                     <a href="">03.</a>
                 </div>
             </div>
+
+
             <!-- Sidebar area -->
             <div class="col-md-4 col-sm-5 sidebar">
                 <!-- Single widget -->
@@ -83,12 +59,14 @@
                 <div class="widget-item">
                     <h2 class="widget-title">Categories</h2>
                     <ul>
-                        <li><a href="#">Vestibulum maximus</a></li>
-                        <li><a href="#">Nisi eu lobortis pharetra</a></li>
-                        <li><a href="#">Orci quam accumsan </a></li>
-                        <li><a href="#">Auguen pharetra massa</a></li>
-                        <li><a href="#">Tellus ut nulla</a></li>
-                        <li><a href="#">Etiam egestas viverra </a></li>
+                        <li>
+                            <?php
+                            $args = [
+                                'title_li' => ''
+                            ];
+                            wp_list_categories($args); //  CSS PROBLEM ON PAGE
+                            ?>
+                        </li>
                     </ul>
                 </div>
                 <!-- Single widget -->
@@ -107,13 +85,28 @@
                 <div class="widget-item">
                     <h2 class="widget-title">Tags</h2>
                     <ul class="tag">
-                        <li><a href="">branding</a></li>
-                        <li><a href="">identity</a></li>
-                        <li><a href="">video</a></li>
-                        <li><a href="">design</a></li>
-                        <li><a href="">inspiration</a></li>
-                        <li><a href="">web design</a></li>
-                        <li><a href="">photography</a></li>
+                        <?php
+                        $args = [
+                            'number' => 9
+                        ];
+                        $tags = $tags_array = get_tags( $args );
+
+                        if ($tags) :
+                        ?>
+                            <ul class="tag">
+                                <?php
+                                foreach ($tags as $tag) :
+                                    // $count++;
+
+                                    echo '<li><a href="' . get_tag_link( $tag->term_id ) . '" title="' . sprintf( __( "" ), $tag->name ) . '" ' . '>' . $tag->name.'</a></li>';
+
+                                    // if( $count > 9 ) break;
+                                endforeach;
+                                ?>
+                            </ul>
+                        <?php
+                        endif;
+                        ?>
                     </ul>
                 </div>
                 <!-- Single widget -->
